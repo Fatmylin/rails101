86a2e7f80edd3dc9e583eb2cbf11eb9e8af16769
Fatmylin <jimmy0921956909@gmail.com>
2018-05-22 12:31:48 +0800

    Fix: 29 rubocop issue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
-
-	def index
-	  flash[:notice] = '安安我是菜鳥工程師Jimmy'
-      flash[:alert] = '有雷勿近'
-	  flash[:warning] = '請大家多多指教'
-	end
+  def index
+    flash[:notice] = '安安我是菜鳥工程師Jimmy'
+    flash[:alert] = '有雷勿近'
+    flash[:warning] = '請大家多多指教'
+  end
 end
