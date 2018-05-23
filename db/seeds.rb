de726fbff33fa265aaaf8a1b5dd8f82ae60100ec
Fatmylin <jimmy0921956909@gmail.com>
2018-05-23 16:38:48 +0800

    ADD: group controller & model & routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+group1 = Group.create(title:'一起來討論', description:'但是我也不知道要討論什麼＝＝')
+group2 = Group.create(title:'那我們來決定野餐地點', description:'ＲＤ都很害怕曬太陽')
