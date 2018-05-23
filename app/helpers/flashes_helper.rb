99e44eec9e7da0d589ff638431623bfc4b6fc6ca
Fatmylin <jimmy0921956909@gmail.com>
2018-05-23 09:44:52 +0800

    Fix: EOF

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,6 +1,5 @@
 module FlashesHelper
-  FLASH_CLASSES = { "alert" => 'danger', "notice" => 'success', "warning" => 'warning' }.freezes
-  
+  FLASH_CLASSES = { "alert" => 'danger', "notice" => 'success', "warning" => 'warning' }.freeze
   def flash_class(key)
     FLASH_CLASSES[key]
   end
