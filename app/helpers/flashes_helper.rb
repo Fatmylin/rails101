7da1b3b4223a49a19979c0c1f65e3961ee6a4672
Fatmylin <jimmy0921956909@gmail.com>
2018-05-23 15:12:50 +0800

    update: flash_helper, use hashwithindifferentaccess

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,11 +1,11 @@
 module FlashesHelper
-  FLASH_CLASSES = { alert: 'danger', notice: 'success', warning: 'warning' }.freeze
+  FLASH_CLASSES = { alert: 'danger', notice: 'success', warning: 'warning' }.with_indifferent_access
 
   def flash_class(key)
-    FLASH_CLASSES[key.to_sym]
+    FLASH_CLASSES[key]
   end
 
   def user_facing_flashes
-    flash.to_hash.slice(*FLASH_CLASSES.keys.map(&:to_s))
+    flash.to_hash.slice(*FLASH_CLASSES.keys)
   end
 end
