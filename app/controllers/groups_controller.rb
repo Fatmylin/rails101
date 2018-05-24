c893ba991121dce396958fa3a0b3d8a1347af536
Fatmylin <jimmy0921956909@gmail.com>
2018-05-24 14:21:30 +0800

    ADD: new action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,30 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new group_params
+
+    if @group.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def delete
+    
+  end
 end
